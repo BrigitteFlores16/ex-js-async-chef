91f03dd9ba642ddc341b0a899ffe4cbbd270dacc
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2025-03-25 18:33:42 +0100

    first commit

---
+++ bonus1/script.js
@@ -0,0 +1,3 @@
+//Bonus 1
+//Attualmente, se la prima richiesta non trova una ricetta, la seconda richiesta potrebbe comunque essere eseguita causando errori a cascata.
+//Modifica getChefBirthday(id) per intercettare eventuali errori prima di fare la seconda richiesta.
